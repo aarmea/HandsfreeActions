87b2af06db6c8ac6a113a16063629f818272fecd
Albert Armea <alvin.armea@gmail.com>
2013-05-26 02:07:46 -0400

    Fix style in RemapperService
    Make 'tag' variable a constant

--- HandsfreeActions/src/main/java/com/albertarmea/handsfreeactions/RemapperService.java
+++ HandsfreeActions/src/main/java/com/albertarmea/handsfreeactions/RemapperService.java
@@ -10,26 +10,26 @@ import android.widget.Toast;
  * Created by aarmea on 5/24/13.
  */
 public class RemapperService extends Service {
-    public String tag = "RemapperService";
+    public static final String TAG = "RemapperService";
 
     @Override
     public void onCreate() {
         super.onCreate();
         Toast.makeText(this, "RemapperService created", Toast.LENGTH_LONG).show();
-        Log.i(tag, "Service created");
+        Log.i(TAG, "Service created");
     }
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
         super.onStartCommand(intent, flags, startId);
-        Log.i(tag, "Service started");
+        Log.i(TAG, "Service started");
         return START_STICKY;
     }
 
     @Override
     public void onDestroy() {
         super.onDestroy();
-        Log.i(tag, "Service stopped");
+        Log.i(TAG, "Service stopped");
         Toast.makeText(this, "RemapperService destroyed", Toast.LENGTH_LONG).show();
     }
 
